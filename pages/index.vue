f38908bebe424806b4feaaf67cbcc41f5d767f93
ZorroDisco <zorro275275@gmail.com>
2026-06-28 12:26:38 +0200

    Creation of the compact tracker for RoT tournament

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useDefaultFont } from "~/hooks/useCustomFont.js";
-const trackers = ["compact", "compactfr", "compactrsl", "compacttp"];
+const trackers = ["compact", "compactfr", "compactrsl", "compactrot", "compacttp"];
 const trackersInfo = ref({});
 const currentHover = ref("");
 const config = useRuntimeConfig();
